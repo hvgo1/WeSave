51ef131c03457ca20749568eddff4a3373581863
jbpabico <j25_jessie@yahoo.com>
2015-03-15 22:38:34 +0800

    added register page

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'mysite/index.html')
+
+def register(request):
+    return render(request,'mysite/register.html')
