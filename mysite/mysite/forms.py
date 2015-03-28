c8c7788ca31405a9d6b6b3bc37ccb65d1c61d951
jbpabico <j25_jessie@yahoo.com>
2015-03-28 16:18:29 +0800

    deleted forms.py

--- mysite/mysite/forms.py
+++
@@ -1,6 +0,0 @@
-from registration.forms import RegistrationFormNoFreeEmail
-
-class RegistrationFormNoFreeEmail(RegistrationForm):
-     
-    bad_domains = ['aim.com', 'aol.com', 'email.com','hushmail.com',
-                   'mail.ru', 'mailinator.com', 'live.com']
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from registration.forms import RegistrationForm
+
+
+class RegistrationFormNoFreeEmail(RegistrationForm):
+     
+    bad_domains = ['aim.com', 'aol.com', 'email.com','hushmail.com',
+                   'mail.ru', 'mailinator.com', 'live.com']
 
 
 # Create your views here.
 def home(request):
     return render(request,'mysite/index.html')
 
-def register(request):
-    return render(request,'mysite/register.html')
