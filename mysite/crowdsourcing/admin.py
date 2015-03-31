ca27dcb45e8ca714e8259cea72fd4b34b95a8f6c
jbpabico <j25_jessie@yahoo.com>
2015-03-31 23:30:02 +0800

    fixed errors

--- mysite/crowdsourcing/admin.py
+++ mysite/crowdsourcing/admin.py
@@ -1,49 +1,61 @@
 from django.contrib import admin
-from croedsourcing.models import UserProfile
-from crowdsourcing.models import Country,Region,City,Barangay,Address,Service_Category,UserProfile,Individual,Group,Campaign,Wish,Keyword,Unregistered_Donor,Contact,Campaign_User_Followers,Campaign_User_Donor,Campaign_Wish,Campaign_Keyword
-# Register your models here.
 
-class AddressAdmin(admin.ModelAdmin):
-    list_display = ['barangay','city','region','country']
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user','address','photo']
-class IndividualAdmin(admin.ModelAdmin):
-    list_display = ['user','first_name','middle_name','last_name','birthday']
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ['user','name','service_category','registration_number']
-class CampaignAdmin(admin.ModelAdmin):
-    list_display = ['title','beneficiary_name','deadline','status','views','created_by']
+from crowdsourcing.models import Country,Region,City,Barangay,Wish,Keyword,Address,Service_Category,UserProfile,Individual,Group,Campaign,Campaign_User_Donor,Campaign_User_Followers,Campaign_Keyword,Unregistered_Donor,Contact,Campaign_Wish
+# Register your models here.
+ 
 class WishAdmin(admin.ModelAdmin):
     list_display = ['name','wish_type']
-class Unregistered_DonorAdmin(admin.ModelAdmin):
-    list_display = ['name','campaign','amount']
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ['name','email','message']
-class Campaign_User_FollowersAdmin(admin.ModelAdmin):
-    list_display = ['campaign','user']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['street','barangay','city','region','country']
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','address','photo']
+
+class IndividualAdmin(admin.ModelAdmin):
+    list_display = ['user','first_name','middle_name','last_name','birthday']
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['user','name','service_category','registration_number']
+
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ['title','beneficiary_name','deadline','status','views','created_by']
+
 class Campaign_User_DonorAdmin(admin.ModelAdmin):
     list_display = ['campaign','user','amount']
+
+class Campaign_User_FollowersAdmin(admin.ModelAdmin):
+    list_display = ['campaign','user']
+
+class Campaign_KeywordAdmin(admin.ModelAdmin):
+    list_display = ['campaign','keyword']
+
+class Unregistered_DonorAdmin(admin.ModelAdmin):
+    list_display = ['name','campaign','amount']
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name','email','message']
+
 class Campaign_WishAdmin(admin.ModelAdmin):
     list_display = ['campaign','wish','completed','estimated_price']
-class Campaign_Keyword(admin.ModelAdmin):
-    list_display = ['campaign','keyword']
+
+
 
 admin.site.register(City)
 admin.site.register(Barangay)
 admin.site.register(Region)
 admin.site.register(Country)
+admin.site.register(Wish,WishAdmin)
+admin.site.register(Keyword)
 admin.site.register(Address,AddressAdmin)
-admin.site.register(UserProfile,UserProfiledmin)
+admin.site.register(Service_Category)
+admin.site.register(UserProfile,UserProfileAdmin)
 admin.site.register(Individual,IndividualAdmin)
 admin.site.register(Group,GroupAdmin)
 admin.site.register(Campaign,CampaignAdmin)
-admin.site.register(Service_Category)
-admin.site.register(Keyword)
-admin.site.register(Wish,WishAdmin)
+admin.site.register(Campaign_User_Donor,Campaign_User_DonorAdmin)
+admin.site.register(Campaign_User_Followers,Campaign_User_FollowersAdmin)
+admin.site.register(Campaign_Keyword,Campaign_KeywordAdmin)
 admin.site.register(Unregistered_Donor,Unregistered_DonorAdmin)
 admin.site.register(Contact,ContactAdmin)
-admin.site.register(Campaign_User_Followers,Campaign_User_FollowersAdmin)
-admin.site.register(Campaign_User_Donor,Campaign_User_DonorAdmin)
 admin.site.register(Campaign_Wish,Campaign_WishAdmin)
-admin.site.register(Campaign_Keyword,Campaign_KeywordAdmin)
-
--- mysite/crowdsourcing/models.py
+++ mysite/crowdsourcing/models.py
@@ -1,7 +1,14 @@
 import datetime
+import os
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   
+MEDIA_PROF = os.path.join(MEDIA_ROOT, 'profile_images')
+
 
 class Country(models.Model):
     def __unicode__(self):  
@@ -23,9 +30,24 @@ class Barangay(models.Model):
         return u'%s' %(self.name)
     name = models.CharField(max_length=200)
 
+class Wish(models.Model):
+    def __unicode__(self): 
+        return u'%s,%s' %(self.name,self.wish_type)
+    STATUS_CHOICES = (
+        ('0', 'Cash'),
+        ('1', 'In-Kind'),
+    )
+    name = models.CharField(max_length=200)
+    wish_type = models.CharField(max_length=10,choices=STATUS_CHOICES)
+
+class Keyword(models.Model):
+    def __unicode__(self): 
+        return u'%s' %(self.name)
+    name = models.CharField(max_length=200)
+
 class Address(models.Model):
     def __unicode__(self):  
-        return u'%s, %s, %s, %s, %s' %(self.street,self.barangay,self.city,self.region,self.country,self.street)    
+        return u'%s,%s,%s,%s,%s' %(self.street,self.barangay,self.city,self.region,self.country)    
     country = models.ForeignKey(Country)  
     region = models.ForeignKey(Region,null=True, blank=True)   
     city = models.ForeignKey(City,null=True, blank=True)    
@@ -40,22 +62,21 @@ class Service_Category(models.Model):
 class UserProfile(models.Model):
     def __unicode__(self): 
         return self.user.id
-     STATUS_CHOICES = (
+    STATUS_CHOICES = (
         ('Ben', 'Beneficiary'),
         ('Hos', 'Hospital_Rep'),
         ('Don', 'Donor'),
         ('Adm', 'Admin'),
     )
-    photo = models.ImageField(upload_to=, null=True, blank=True)
+    
+    photo = models.ImageField(upload_to=MEDIA_PROF, null=True, blank=True)
     address = models.ForeignKey(Address)
     user = models.OneToOneField(User)
-    role = models.models.CharField(max_length=15,choices=STATUS_CHOICES)
-
-   
+    role = models.CharField(max_length=15,choices=STATUS_CHOICES)
 
 class Individual(models.Model):
     def __unicode__(self): 
-        return u'%s,%s,%s' %(self.first_name,self.middle_name,self.last_name)#user
+        return self.user.id
     first_name = models.CharField(max_length=200)
     middle_name = models.CharField(max_length=200,null=True,blank=True)
     last_name = models.CharField(max_length=200)
@@ -64,13 +85,13 @@ class Individual(models.Model):
 
 class Group(models.Model):
     def __unicode__(self): 
-        return u'%s,%s' %(self.user,self.name)
+        return self.user.id
     name = models.CharField(max_length=200)
     page_address = models.URLField(max_length=200,null=True, blank=True)
     about = models.CharField(max_length=200)
     service_category = models.ForeignKey(Service_Category)
     registration_number = models.BigIntegerField(null=True, blank=True)
-    document = FileField(upload_to =None,null=True, blank=True)
+    document = models.FileField(upload_to =None,null=True, blank=True)
     comments = models.CharField(max_length=200, null=True, blank=True)
     pc_first_name = models.CharField(max_length=200)
     pc_last_name = models.CharField(max_length=200)
@@ -104,70 +125,52 @@ class Campaign(models.Model):
     ack_receipt = models.ImageField(upload_to=None, null=True, blank=True)
     campaign_image = models.ImageField(upload_to=None, null=True, blank=True)
     created_by = models.ForeignKey(User)
-    donors = models.ManyToManyField(User,through='Campaign_User_Donor',through_fields=('campaign', 'user'))
-    subscribers = models.ManyToManyField(User,through='Campaign_User_Followers',through_fields=('campaign', 'user'))
-   wishes = models.ManyToManyField(Wish,through='Campaign_Wish',through_fields=('campaign', 'wish'))
-   keywords = models.ManyToManyField(Keyword,through='Campaign_Keywords',through_fields=('campaign', 'keyword'))
+    donors = models.ManyToManyField(User,through='Campaign_User_Donor',through_fields=('campaign','user'),related_name='campaign_donors')
+    subscribers = models.ManyToManyField(User,through='Campaign_User_Followers',through_fields=('campaign', 'user'),related_name='campaign_subscribers')
+    wishes = models.ManyToManyField(Wish,through='Campaign_Wish')
+    keywords = models.ManyToManyField(Keyword,through='Campaign_Keyword')
     # set default value of approval_tag to false
-
-class Wish(models.Model):
-    def __unicode__(self): 
-        return u'%s,%s' %(self.name,self.wish_type)
-    STATUS_CHOICES = (
-        ('0', 'Cash'),
-        ('1', 'In-Kind'),
-    )
-    name = models.CharField(max_length=200)
-    wish_type = models.CharField(max_length=10,choices=STATUS_CHOICES)
-
-class Keyword(models.Model):
-    def __unicode__(self): 
-        return u'%s' %(self.name)
-    name = models.CharField(max_length=200)
 
 class Unregistered_Donor(models.Model):
     def __unicode__(self): 
         return u'%s' %(self.name)
     name = models.CharField(max_length=200)
     campaign = models.ForeignKey(Campaign)
-    amount = models.DecimalField(decimal_places=2)
+    amount = models.DecimalField(max_digits=20,decimal_places=2)
 
 class Contact(models.Model):
     def __unicode__(self): 
         return u'%s,%s,%s' %(self.name,self.email,self.message)
     name = models.CharField(max_length=200)
-    email = models.models.EmailField(max_length=75)
+    email = models.EmailField(max_length=75)
     message = models.CharField(max_length=200)
-
-class Campaign_User_Followers(models.Model):
-    def __unicode__(self): 
-        return u'%s,%s' %(self.campaign,self.user)
-    campaign = models.ForeignKey(Campaign)
-    user = models.ForeignKey(User)
 
 class Campaign_User_Donor(models.Model):
     def __unicode__(self): 
-        return u'%s,%s,%s' %(self.campaign,self.user,self.amount)
+        return self.id
     campaign = models.ForeignKey(Campaign)
     user = models.ForeignKey(User)
-    amount = models.DecimalField(decimal_places=2)
+    amount = models.DecimalField(max_digits=20,decimal_places=2)
 
 class Campaign_Wish(models.Model):
     def __unicode__(self): 
-        return u'%s,%s,%s,%s' %(self.campaign,self.wish,self.completed,self.estimated_price)
+        return self.id
     campaign = models.ForeignKey(Campaign)
     wish = models.ForeignKey(Wish)
     completed = models.BooleanField()
-    estimated_price = models.DecimalField(decimal_places=2)
+    estimated_price = models.DecimalField(max_digits=20,decimal_places=2)
     # set default value of received_tag to false
+
+class Campaign_User_Followers(models.Model):
+    def __unicode__(self): 
+        return self.id
+    campaign = models.ForeignKey(Campaign)
+    user = models.ForeignKey(User)
 
 class Campaign_Keyword(models.Model):
     def __unicode__(self): 
-        return u'%s,%s' %(self.campaign,self.keyword)
+        return self.id
     campaign = models.ForeignKey(Campaign)
     keyword = models.ForeignKey(Keyword)
-   
-
-
-
+ 
 
