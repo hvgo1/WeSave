fc99722891e9748066c5ce08b4502be90a2c33ad
jbpabico <j25_jessie@yahoo.com>
2015-04-01 16:04:57 +0800

    Edited models and admin.py

--- mysite/crowdsourcing/admin.py
+++ mysite/crowdsourcing/admin.py
@@ -1,34 +1,61 @@
 from django.contrib import admin
-from crowdsourcing.models import Location,User,Individual,Group,Role,Campaign,WishType,Wishlist,User_Role,Campaign_User_Followers,Campaign_Wishlist
 
+from crowdsourcing.models import Country,Region,City,Barangay,Wish,Keyword,Address,Service_Category,UserProfile,Individual,Group,Campaign,Campaign_User_Donor,Campaign_User_Followers,Campaign_Keyword,Unregistered_Donor,Contact,Campaign_Wish
 # Register your models here.
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ['barangay','city','country']
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['email','password','location']
-class IndividualAdmin(admin.ModelAdmin):
-    list_display = ['first_name','middle_name','last_name','birthday','user']
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ['name','user']
-class CampaignAdmin(admin.ModelAdmin):
-    list_display = ['title','beneficiary_name','story','deadline','approval_tag','status','views','created_by']
-class WishlistAdmin(admin.ModelAdmin):
+ 
+class WishAdmin(admin.ModelAdmin):
     list_display = ['name','wish_type']
-class User_RoleAdmin(admin.ModelAdmin):
-    list_display = ['user','role']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['street','barangay','city','region','country']
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','address','photo']
+
+class IndividualAdmin(admin.ModelAdmin):
+    list_display = ['user','first_name','middle_name','last_name','birthday']
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['user','name','service_category','registration_number']
+
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ['title','beneficiary_name','deadline','status','views','created_by']
+
+class Campaign_User_DonorAdmin(admin.ModelAdmin):
+    list_display = ['campaign','user','amount']
+
 class Campaign_User_FollowersAdmin(admin.ModelAdmin):
     list_display = ['campaign','user']
-class Campaign_WishlistAdmin(admin.ModelAdmin):
-    list_display = ['campaign','wishlist','received_tag']
 
-admin.site.register(Location,LocationAdmin)
-admin.site.register(User,UserAdmin)
+class Campaign_KeywordAdmin(admin.ModelAdmin):
+    list_display = ['campaign','keyword']
+
+class Unregistered_DonorAdmin(admin.ModelAdmin):
+    list_display = ['name','campaign','amount']
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name','email','message']
+
+class Campaign_WishAdmin(admin.ModelAdmin):
+    list_display = ['campaign','wish','completed','estimated_price']
+
+
+
+admin.site.register(City)
+admin.site.register(Barangay)
+admin.site.register(Region)
+admin.site.register(Country)
+admin.site.register(Wish,WishAdmin)
+admin.site.register(Keyword)
+admin.site.register(Address,AddressAdmin)
+admin.site.register(Service_Category)
+admin.site.register(UserProfile,UserProfileAdmin)
 admin.site.register(Individual,IndividualAdmin)
 admin.site.register(Group,GroupAdmin)
-admin.site.register(Role)
 admin.site.register(Campaign,CampaignAdmin)
-admin.site.register(WishType)
-admin.site.register(Wishlist,WishlistAdmin)
-admin.site.register(User_Role,User_RoleAdmin)
+admin.site.register(Campaign_User_Donor,Campaign_User_DonorAdmin)
 admin.site.register(Campaign_User_Followers,Campaign_User_FollowersAdmin)
-admin.site.register(Campaign_Wishlist,Campaign_WishlistAdmin)
+admin.site.register(Campaign_Keyword,Campaign_KeywordAdmin)
+admin.site.register(Unregistered_Donor,Unregistered_DonorAdmin)
+admin.site.register(Contact,ContactAdmin)
+admin.site.register(Campaign_Wish,Campaign_WishAdmin)
