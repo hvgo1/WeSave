a1fa61a2be436504acef8a277aca625cb57f8d74
jbpabico <j25_jessie@yahoo.com>
2015-04-30 19:04:17 +0800

    added description

--- mysite/manage_contacts/forms.py
+++ mysite/manage_contacts/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 from crowdsourcing.models import Contact
 
-class ContactForm(forms.ModelForm):
+#Form for contact model
+class ContactForm(forms.ModelForm): 
     class Meta:
         model = Contact
         fields = ('name','email','message')
