752a0eb4dc63a9263d33b87b6287d90b6dccd64f
jbpabico <j25_jessie@yahoo.com>
2015-05-25 17:35:34 +0800

    update profil working e

--- mysite/maintain_profile/views.py
+++ mysite/maintain_profile/views.py
@@ -3,9 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import UpdateView
 from django.core.paginator import Paginator, InvalidPage,EmptyPage, PageNotAnInteger
-
 from django.contrib.auth.models import User
-#from maintain_profile.forms import 
 from crowdsourcing.models import UserProfile,Individual,Group,CampaignUserDonor,CampaignUserFollowers,Campaign
 from crowdsourcing.forms import UserProfileForm, IndividualForm,GroupForm
 
@@ -25,6 +23,7 @@ def view_profile(request,username):
         is_indiv = False
     return render(request,'maintain_profile/viewprofile.html',{'user':user,'created':created,'sub':sub,'donor':donor,'profile':profile,'details':details,'is_indiv':is_indiv})
 
+#Update profiles of users
 def update_profile(request,username): 
     user = User.objects.get(username=username)
     profile = UserProfile.objects.get(user_id=user.id) 
@@ -38,9 +37,9 @@ def update_profile(request,username):
             individual_form = IndividualForm(request.POST,instance=individual_details)  
         if user_profile_form.is_valid(): 
             profile.photo=request.FILES.get('photo','profile_images/def.jpg')
-            profile.save() 
             if individual_form.is_valid():
-                individual_details.save()                          
+                individual_details.save()  
+                profile.save()                         
                 return HttpResponseRedirect('/profile/'+username+'/')
         return render(request,'maintain_profile/updateprofile.html', {'user_profile_form': user_profile_form,'individual_form': individual_form,'action':'update/'+ username})
 
@@ -54,31 +53,17 @@ def update_profile(request,username):
             group_form = GroupForm(request.POST,instance=group_details)  
         if user_profile_form.is_valid(): 
             profile.photo=request.FILES.get('photo','profile_images/def.jpg')
-            profile.save() 
+            
             if group_form.is_valid():
-                #group_details.document=request.FILES.get('document', None) 
-                #group_details.page_address=request.POST.get('page_address',None)
-                #group_details.registration_number=request.POST.get('registration_number',None)
-                #group_details.comments=request.POST.get('comments',None),
-                group_details.name=request.POST['name']
-                group_details.page_address=request.POST.get('page_address',None) #or ""Blank
-                group_details.about=request.POST['about']
-                group_details.service_category=request.POST['service_category']
-                group_details.registration_number=int(request.POST.get('registration_number',None))
-                group_details.document=request.FILES.get('document', None)
-                group_details.comments=request.POST.get('comments',None)
-                group_details.pc_first_name = request.POST['pc_first_name']
-                group_details.pc_last_name = request.POST['pc_last_name']
-                group_details.pc_email = request.POST['pc_email']
-                group_details.pc_job_title = request.POST['pc_job_title']
-                group_details.pc_phone_number = request.POST['pc_phone_number']
-                group_details.sc_first_name = request.POST['sc_first_name']
-                group_details.sc_last_name = request.POST['sc_last_name']
-                group_details.sc_email = request.POST['sc_email']
-                group_details.sc_job_title = request.POST['sc_job_title']
-                group_details.sc_phone_number = request.POST['sc_phone_number']
-                group_details.user_id=user.id
-                group_details.save()            
+                          
+                for category in group_details.service_category.all():
+                    category.delete()
+                categories = group_form.cleaned_data['service_category']
+                for category in categories:
+                    group_details.service_category.add(category)
+
+                group_details.save()   
+                profile.save()          
             return HttpResponseRedirect('/profile/'+username+'/')
     return render(request, 'maintain_profile/updateprofile.html', {'user_profile_form':user_profile_form, 'group_form':group_form})
    
