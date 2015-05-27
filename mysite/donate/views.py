a0d3b2d6ed2684d127789654bf16124f41b0e991
jbpabico <j25_jessie@yahoo.com>
2015-05-27 14:43:34 +0800

    inkind.html fixed

--- mysite/donate/views.py
+++ mysite/donate/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from donate.forms import InkindForm
-from crowdsourcing.models import Campaign
+from django.contrib.auth.models import User
+from crowdsourcing.models import Campaign,Individual,Group
 
 def donate_inkind(request,campaign_title_slug):
     campaign = Campaign.objects.get(slug=campaign_title_slug)
     if request.method == 'GET':
         inkind_form = InkindForm()
         if request.user.is_authenticated():
-            
+            username = request.user.get_username()
+            user_object = User.objects.get(username=username)
+            if Individual.objects.filter(user_id=user_object.id).exists():
+                    is_individual = True
+            else:
+                    is_individual = False
             is_logged_in = True
             #username = request.user.get_username()
             
@@ -26,9 +32,11 @@ def donate_inkind(request,campaign_title_slug):
                 profile = UserProfile.objects.get(user_id=user_object.id)
    
                 if Individual.objects.filter(user_id=user_object.id).exists():
+                    is_individual = True
                     member = Individual.objects.get(user_id=user_object.id)
                     inkind_object.pc_contact_number = member.phone_number
                 else:
+                    is_individual = False
                     member = Group.objects.get(user_id=user_object.id)
                     inkind_object.pc_contact_number = member.pc_contact_number
                     inkind_object.sc_contact_number = member.sc_contact_number
@@ -44,4 +52,4 @@ def donate_inkind(request,campaign_title_slug):
 
             inkind_object.save()          
             return HttpResponseRedirect('/home/')
-    return render(request,'donate/inkind.html',{'inkind_form':inkind_form,'is_logged_in':is_logged_in})
+    return render(request,'donate/inkind.html',{'inkind_form':inkind_form,'is_logged_in':is_logged_in,'is_individual':is_individual})
