d28fcee123995b39e86d9045b382e33ecdb1daea
jbpabico <j25_jessie@yahoo.com>
2015-05-29 16:24:38 +0800

    display form reallocation

--- mysite/maintain_campaign/urls.py
+++ mysite/maintain_campaign/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from maintain_campaign import views
+import reallocate_donation 
 
 urlpatterns = patterns('', 
     #url(r'^$', views.index, name='index'),
@@ -7,5 +8,7 @@ urlpatterns = patterns('',
     url(r'^view/(?P<campaign_title_slug>[\w\-]+)/$', views.viewCampaign, name='view_campaign'),
     url(r'^update/(?P<id>\d+)/$', views.updateCampaign, name='update_campaign'),
     url(r'^list/$', views.listCampaign, name='list_campaign'),
+    url(r'^view-details/reallocate/(?P<id>\d+)/$', include('reallocate_donation.urls'), name='reallocate'),
     url(r'^view-details/$', views.viewCampaignDetails, name='view_campaign_details'),
+    
     )
