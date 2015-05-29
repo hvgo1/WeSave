c0abab331b386e4ebd60ed034667990f90351d45
jbpabico <j25_jessie@yahoo.com>
2015-05-29 11:00:21 +0800

    Complete inkind form

--- mysite/donate/forms.py
+++ mysite/donate/forms.py
@@ -9,7 +9,7 @@ class InkindForm1(forms.ModelForm):
     sc_contact_number = forms.CharField(max_length=128,label="Secondary Contact Number")
     description = forms.CharField(widget=forms.Textarea,help_text = "Please input a description of your donation.")
     reason = forms.CharField(widget=forms.Textarea,help_text = "Please input a reason for your donation.")
-    fair_market_value = forms.DecimalField(widget=TextInput(attrs={'type':'number'}),max_digits=20,decimal_places=2)
+    fair_market_value = forms.DecimalField(max_digits=20,decimal_places=2)
     class Meta:
         model = InkindDonation
         exclude = ('campaign',)
@@ -20,7 +20,7 @@ class InkindForm2(forms.ModelForm):
     sc_contact_number = forms.CharField(max_length=128,label="Secondary Contact Number")
     description = forms.CharField(widget=forms.Textarea,help_text = "Please input a description of your donation.")
     reason = forms.CharField(widget=forms.Textarea,help_text = "Please input a reason for your donation.")
-    fair_market_value = forms.DecimalField(widget=TextInput(attrs={'type':'number'}),max_digits=20,decimal_places=2)
+    fair_market_value = forms.DecimalField(max_digits=20,decimal_places=2)
     class Meta:
         model = InkindDonation
         exclude = ('campaign',)
@@ -30,7 +30,7 @@ class InkindForm3(forms.ModelForm):
     
     description = forms.CharField(widget=forms.Textarea,help_text = "Please input a description of your donation.")
     reason = forms.CharField(widget=forms.Textarea,help_text = "Please input a reason for your donation.")
-    fair_market_value = forms.DecimalField(widget=TextInput(attrs={'type':'number'}),max_digits=20,decimal_places=2)
+    fair_market_value = forms.DecimalField(max_digits=20,decimal_places=2)
     class Meta:
         model = InkindDonation
         exclude = ('campaign',)
--- mysite/donate/views.py
+++ mysite/donate/views.py
@@ -12,45 +12,31 @@ def donate_inkind(request,campaign_title_slug):
             username = request.user.get_username()
             user_object = User.objects.get(username=username)
             if Individual.objects.filter(user_id=user_object.id).exists():
-                    is_individual = True
-                    print "indiv"
+ 
                     inkind_form = InkindForm2()
             elif Group.objects.filter(user_id=user_object.id).exists():
-                    print "group"
-                    is_individual = False
+
                     inkind_form = InkindForm3()
-            is_logged_in = True
-            if is_logged_in == True:
-                    print "true"
-            #username = request.user.get_username()
-            
+  
         else:
-            is_individual = False
-            inkind_form = InkindForm1()
-            is_logged_in = False
-            if is_logged_in == False:
-                    print "F"
         
+            inkind_form = InkindForm1()
+   
     else:
 
-        print "post"
- 
         if request.user.is_authenticated():
-            is_logged_in = True
-            if is_logged_in == True:
-                print "true" 
+ 
             username = request.user.get_username()
             user_object = User.objects.get(username=username)
-            print user_object.username
+         
             profile = UserProfile.objects.get(user_id=user_object.id)
    
             if Individual.objects.filter(user_id=user_object.id).exists():
-                print "indiv" 
-                is_individual = True
+      
                 inkind_form = InkindForm2(request.POST)
                 
                 if inkind_form.is_valid():
-                    print "valid"
+                   
                     inkind_object = inkind_form.save(commit=False)
                     member = Individual.objects.get(user_id=user_object.id)
                     inkind_object.pc_contact_number = member.phone_number
@@ -69,15 +55,9 @@ def donate_inkind(request,campaign_title_slug):
                     inkind_object.address = profile.street +  unicode(barangay) + unicode(city) + unicode(profile.country.name)
                     inkind_object.campaign_id = campaign.id
                     inkind_object.save() 
-                    print inkind_object.pc_contact_number
-                    print inkind_object.name
-                    print inkind_object.email
-                    print inkind_object.address
-                    print inkind_object.campaign_id
-                    print is_logged_in
+           
             else:
-                print "group" 
-                is_individual = False
+              
                 inkind_form = InkindForm3(request.POST)
                 
                 if inkind_form.is_valid():
@@ -99,22 +79,20 @@ def donate_inkind(request,campaign_title_slug):
                     inkind_object.address = profile.street +  unicode(barangay) + unicode(city) + unicode(profile.country.name)
                     inkind_object.campaign_id = campaign.id
                     inkind_object.save() 
-                    print is_logged_in
+                  
 
             
         else:
-            is_individual = False
-            is_logged_in = False
+         
             inkind_form = InkindForm1(request.POST)
             
             if inkind_form.is_valid():
                 inkind_object = inkind_form.save(commit=False)
-                if is_logged_in == False:
-                    print "F"
+                
                 unregistered_donor = UnregisteredDonor.objects.get_or_create(name=request.POST['name'],
                                      campaign_id=campaign.id,
                                      amount=request.POST['fair_market_value'])[0]
                 inkind_object.campaign_id = campaign.id
                 inkind_object.save()          
         return HttpResponseRedirect('/home/')
-    return render(request,'donate/inkind.html',{'inkind_form':inkind_form,'is_logged_in':is_logged_in,'is_individual':is_individual})
+    return render(request,'donate/inkind.html',{'inkind_form':inkind_form})
